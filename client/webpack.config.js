e7392c7e39611b3e3b600483afe1532a5779ec45
Connor812 <connorsavoy812@gmail.com>
2023-03-16 20:14:18 -0400

    added a new plugin to webpack called copyWebpackPlugin that adds my newly created sized photos to a folder called assets/icons inside of the dist folder

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = () => {
   return {
@@ -17,15 +15,15 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin',
       }),
-      new MiniCssExtractPlugin(),
       new InjectManifest({
-        swSrc: './src/sw.js',
-        swDest: 'service-worker.js',
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
       }), 
       new WebpackPwaManifest({
         name: 'Text Editor',
@@ -38,9 +36,25 @@ module.exports = () => {
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: 'assets/icons',
+            ios: true,
+            purpose: 'any maskable',
+            rename: 'icon-[width].[ext]'
           },
         ]
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: 'src/images',
+            to: path.resolve(__dirname, 'dist/assets/icons'),
+            globOptions: {
+              ignore: ['**/.*'], // ignore dot files and directories
+            },
+            noErrorOnMissing: true, // don't throw error if files don't exist
+          },
+        ],
       }),
     ],
 
@@ -49,6 +63,15 @@ module.exports = () => {
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          loader: 'responsive-loader',
+          options: {
+            outputPath: 'assets/icons',
+            name: 'icon-[width].[ext]',
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
         },
         {
           test: /\.m?js$/,
