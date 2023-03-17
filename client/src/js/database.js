10bc3bdaac66b9404a3b088a5137f958fee7a01f
Connor812 <connorsavoy812@gmail.com>
2023-03-16 22:40:02 -0400

    got the database to work

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,50 +1,38 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('text-editor', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('text-editor')) {
+        console.log('text-editor database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('text-editor', { keyPath: 'id', autoIncrement: true });
+      console.log('text-editor database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const DBname = 'text editor';
   console.log('Put to the ase');
-  const TextDB = await openDB(DBname, 1);
-  console.log(TextDB)
-  const tx = TextDB.transaction(DBname, 'readwrite');
-  console.log(tx)
-  const store = tx.objectStore(DBname);
-  console.log(store)
+  const TextDB = await openDB('text-editor', 1);
+  const tx = TextDB.transaction('text-editor', 'readwrite');
+  const store = tx.objectStore('text-editor');
   const request = store.add({ Text: content });
-  console.log(request)
   const result = await request;
-  console.log(result)
   console.log('🚀 - data saved to the database', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  const DBname = 'text editor';
-console.log('GET all from the database');
-const TextDB = await openDB(DBname, 1);
-console.log(TextDB)
-const tx = TextDB.transaction(DBname, 'readonly');
-console.log(tx)
-const store = tx.objectStore(DBname);
-console.log(store)
-const request = store.getAll();
-console.log(request)
-const result = await request;
-console.log(request)
-console.log('result.value', result);
-return result;
+  console.log('GET all from the database');
+  const TextDB = await openDB('text-editor', 1);
+  const tx = TextDB.transaction('text-editor', 'readonly');
+  const store = tx.objectStore('text-editor');
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 }
 
 initdb();
